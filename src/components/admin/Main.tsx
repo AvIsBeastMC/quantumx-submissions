9a261c1f0eaa12441c84f5219af606cbb4d7dccf
Arunnya Varma <me@arunnya.com>
2024-12-26 22:04:24 +0530

    bet

---
+++ src/components/admin/Main.tsx
@@ -0,0 +1,192 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react"
+import { atom, useAtom } from 'jotai';
+import { api } from "~/utils/api";
+import { cn } from "@nextui-org/react";
+import { ShadowsIntoLight } from "~/pages/_app";
+import { AnimatePresence, motion } from "framer-motion";
+import { ArrowLeft, ChevronLeft, CloudDownload, LinkIcon } from "lucide-react";
+import Link from "next/link";
+
+const modeAtom = atom<{
+  mode: 'list' | 'data',
+  data?: any
+}>({
+  mode: 'data',
+  data: 'Cinematique'
+});
+
+type SE = 'cinematique' | 'framesperframe' | 'kanvas' | 'mindmaze' | 'pitchathon' | 'reimagine' | 'render' | 'thebeathive' | 'webwaves'
+
+const categories: {
+  name: string,
+  id: SE,
+}[] = [{
+  id: 'cinematique',
+  name: 'Cinematique',
+}, {
+  id: 'framesperframe',
+  name: 'Frames Per Frame'
+}, {
+  id: 'kanvas',
+  name: 'Kanvas'
+}, {
+  id: 'mindmaze',
+  name: 'Mindmaze'
+}, {
+  id: 'pitchathon',
+  name: 'Pitchathon'
+}, {
+  id: 'reimagine',
+  name: 'Re-Imagine',
+}, {
+  id: 'render',
+  name: 'Render'
+}, {
+  id: 'thebeathive',
+  name: 'The Beat Hive'
+}, {
+  id: 'webwaves',
+  name: 'Web Waves'
+}]
+
+const List = () => {
+  const [mode, setMode] = useAtom(modeAtom);
+
+  const select = (s: SE) => {
+    setMode({
+      mode: 'data',
+      data: categories.find((c) => c.id == s)!.name
+    })
+  }
+
+  return (
+    <motion.div key="list" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <main className="backdrop-blur-sm flex min-h-screen flex-col items-center justify-center main py-12">
+        <div className='mx-auto md:w-2/4 px-6'>
+          <h1 className='text-3xl font-bold mb-12'><span className='border-b-2 border-dotted'>SELECT EVENT</span></h1>
+
+          <div className='flex flex-col'>
+            {categories.map((c, i) => (
+              <button onClick={() => select(c.id)} key={i} className='flex flex-row gap-4 items-center hover:scale-105 transition-all'>
+                ○
+                <div className='border-b-2 border-dotted w-full py-6 self-left align-left'>
+                  <h1 className={cn('text-4xl font-bold tracking-widest text-left', ShadowsIntoLight.className)}>{c.name}</h1>
+                </div>
+              </button>
+            ))}
+          </div>
+        </div>
+      </main>
+    </motion.div>
+  )
+}
+
+const Data = () => {
+  const [mode, setMode] = useAtom(modeAtom);
+  const { data, error } = api.admin.list.useQuery(typeof mode.data == 'string' ? mode.data : '');
+
+  if (!data) return <></>;
+
+  const submissions = {
+    individual: data.filter(d => d.moreData?.school.type == 'INDIVIDUAL'),
+    school: data.filter(d => d.moreData?.school.type == 'SCHOOL'),
+    unidentified: data.filter(d => !d.moreData),
+  }
+
+  return (
+    <motion.div className="w-full" key="data" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <div className="w-2/3 flex flex-col mx-auto py-12">
+        <button onClick={() => {
+          setMode({
+            mode: 'list',
+            data: undefined
+          })
+        }} className="flex flex-row gap-2 items-center mb-2"><ArrowLeft size={16} /> GO BACK</button>
+        <h1 className="font-bold text-4xl">
+          <span className="border-b-2 border-gray-200">{mode.data}</span>
+        </h1>
+        <p className="mt-3"><b>SUBMISSIONS:</b> {submissions.individual.length} Individual, {submissions.school.length} School</p>
+
+        <div className="w-full py-6">
+          <h1 className="font-medium text-2xl mb-2">
+            <span className="border-b-2 border-dotted border-gray-200">Individual</span>
+          </h1>
+          <div className="w-full md:w-3/4 py-2 flex flex-col gap-2">
+            {submissions.individual.map((s, i) => (
+              <div key={i} className="rounded-xl border-2 border-gray-100 px-4 py-2 w-full flex flex-col">
+                <div className="mr-auto">
+                  <h1 className="text-xl font-bold">{s.moreData?.schoolId ?? <span className="text-red-300">⚠️ Registration Not Found</span>}</h1>
+                  <div className="flex flex-col mt-1">
+                    <span><span className="font-medium">Project title:</span> {s.title}</span>
+                    <span><span className="font-medium">Submitted by:</span> {s.moreData?.students.map((s) => s.name).join(', ')}</span>
+                  </div>
+                </div>
+                <div className="mt-2 flex flex-row gap-2 items-center">
+                  {s.files.map((f, i) => <Link target="_blank" href={`https://static-files.thequantumx.xyz/submissions/${s.folderId}/${f}`} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><CloudDownload size={16} /> File</Link>)}
+                  {s.links.map((l, i) => <Link target="_blank" href={l} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><LinkIcon size={16} /> Link</Link>)}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-full py-3">
+          <h1 className="font-medium text-2xl mb-2">
+            <span className="border-b-2 border-dotted border-gray-200">School</span>
+          </h1>
+          <div className="w-full md:w-3/4 py-2 flex flex-col gap-2">
+            {submissions.individual.map((s, i) => (
+              <div key={i} className="rounded-xl border-2 border-gray-100 px-4 py-2 w-full flex flex-col">
+                <div className="mr-auto">
+                  <h1 className="text-xl font-bold">{s.moreData?.schoolId ?? <span className="text-red-300">⚠️ Registration Not Found</span>}</h1>
+                  <div className="flex flex-col mt-1">
+                    <span><span className="font-medium">Project title:</span> {s.title}</span>
+                    <span><span className="font-medium">Submitted by:</span> {s.moreData?.students.map((s) => s.name).join(', ')}</span>
+                  </div>
+                </div>
+                <div className="mt-2 flex flex-row gap-2 items-center">
+                  {s.files.map((f, i) => <Link target="_blank" href={`https://static-files.thequantumx.xyz/submissions/${s.folderId}/${f}`} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><CloudDownload size={16} /> File</Link>)}
+                  {s.links.map((l, i) => <Link target="_blank" href={l} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><LinkIcon size={16} /> Link</Link>)}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {(submissions.unidentified.length > 0) && (
+          <div className="w-full py-3">
+            <h1 className="font-medium text-2xl mb-2">
+              <span className="border-b-2 border-dotted border-gray-200">⚠️ Unidentified</span>
+            </h1>
+            <div className="w-full md:w-3/4 py-2 flex flex-col gap-2">
+              {submissions.individual.map((s, i) => (
+                <div key={i} className="rounded-xl border-2 border-gray-100 px-4 py-2 w-full flex flex-col">
+                  <div className="mr-auto">
+                    <h1 className="text-xl font-bold">{s.moreData?.schoolId ?? <span className="text-red-300">⚠️ Registration Not Found</span>}</h1>
+                    <div className="flex flex-col mt-1">
+                      <span><span className="font-medium">Project title:</span> {s.title}</span>
+                      <span><span className="font-medium">Submitted by:</span> {s.moreData?.students.map((s) => s.name).join(', ')}</span>
+                    </div>
+                  </div>
+                  <div className="mt-2 flex flex-row gap-2 items-center">
+                    {s.files.map((f, i) => <Link target="_blank" href={`https://static-files.thequantumx.xyz/submissions/${s.folderId}/${f}`} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><CloudDownload size={16} /> File</Link>)}
+                    {s.links.map((l, i) => <Link target="_blank" href={l} key={i} className="hover:bg-gray-100 transition-colors flex flex-row gap-1 px-2 py-1 rounded-md border-1 border-gray-200 items-center gap-2"><LinkIcon size={16} /> Link</Link>)}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  )
+}
+
+export default function MainMode() {
+  const [mode, setMode] = useAtom(modeAtom);
+
+  return (
+    <AnimatePresence mode="wait">
+      {mode.mode == 'list' ? <List /> : <Data />}
+    </AnimatePresence>
+  )
+}
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import toast from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import MainMode from '~/components/admin/Main';
 import { api } from '~/utils/api';
 
 const Dashboard = () => {
@@ -7,14 +8,16 @@ const Dashboard = () => {
   const [authenticated, setAuthenticated] = useState<boolean>(false)
 
   const LoginMode = () => {
-    const {mutate} = api.admin.login.useMutation();
-                              
+    const { mutate } = api.admin.login.useMutation();
+
     const [usernameInput, setUsernameInput] = useState<string>()
     const [passwordInput, setPasswordInput] = useState<string>();
 
     const [loading, setLoading] = useState<boolean>(false)
 
-    const login = () => {
+    const login = (e: React.FormEvent) => {
+      e.preventDefault();
+
       if (!usernameInput || !passwordInput) return toast.error('Please enter both username and password.');
 
       setLoading(true);
@@ -24,7 +27,12 @@ const Dashboard = () => {
         password: passwordInput
       }, {
         onSuccess(d) {
-          
+          if (d == true) {
+            setAuthenticated(true);
+            setMode('main')
+          } else {
+            return toast.error('Bad Credentials')
+          }
         },
         onError(e) {
           setLoading(false);
@@ -42,20 +50,24 @@ const Dashboard = () => {
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Enter username and password below.</p>
             </div>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
-              <div className="flex flex-wrap -m-2">
+              <form onSubmit={login} className="flex flex-wrap -m-2">
                 <div className="p-2 w-1/2">
                   <div className="relative">
-                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Username</label>
+                    <input onChange={(e) => setUsernameInput(e.currentTarget.value)} required type="text" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                   </div>
                 </div>
                 <div className="p-2 w-1/2">
                   <div className="relative">
-                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">Password</label>
+                    <input onChange={(e) => setPasswordInput(e.currentTarget.value)} required type="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                   </div>
                 </div>
-              </div>
+
+                <button type='submit' className='hover:opacity-75 transition-all font-bold px-4 py-2 rounded-md bg-gray-200 flex mx-auto mt-4'>
+                  LOGIN
+                </button>
+              </form>
             </div>
           </div>
         </section>
@@ -63,15 +75,11 @@ const Dashboard = () => {
     )
   };
 
-  const MainMode = () => {
-    return (
-      <></>
-    )
-  }
-
-
   return (
-    mode == 'login' ? <LoginMode /> : <MainMode />
+    <>
+      {mode == 'login' ? <LoginMode /> : <MainMode />}
+      <Toaster />
+    </>
   )
 }
 
